59b7fe72946e34bfee8e47683eec90bfb70d7635
Emmet <emmet.memery@outlook.com>
2013-06-06 15:06:14 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bpspsych::Application.config.secret_token = 'a54e4514dfd5cee5e86e24592d18caa254a666f24c5bad14053b42796546073ab0ff760f91aa15c03163903b071d8a2ce5d453ebe57b0b5daae314f4ce440111'
